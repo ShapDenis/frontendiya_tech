2ceae8f5ce5ea3528cb5da591897aa9a6f0bb87e
denisshapochkin <denis@getfocalpoint.com>
2024-06-27 16:51:33 +0300

    feat: connect back

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
 import "./App.css";
 import Header from "./components/Header/Header.tsx";
+import EmptyResult from "./components/EmptyResult/EmptyResult.tsx";
+import searchIcon from "../src/assets/search.svg";
+import {useEffect, useState} from "react";
+import services from "./services";
+import User from "./components/User/User.tsx";
+import {UserTypes} from "./components/User/type.ts";
 
 function App() {
-	return (
-		<>
-			<Header />
-		</>
-	);
+  const [search, setSearch] = useState<string>("");
+  const [data, setData] = useState<UserTypes | null>(null);
+  const [timeoutId, setTimeoutId] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (timeoutId) {
+      clearTimeout(timeoutId); // Clear previous timeout
+    }
+
+    const newTimeoutId = setTimeout(() => {
+      services.fetchData.getUser(search)
+      .then((res) => setData(res))
+      .catch(() => setData(null));
+    }, 1000); // 1-second delay
+
+    setTimeoutId(newTimeoutId);
+
+    // Cleanup function to clear timeout on component unmount or search change
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [search]);
+
+  return (
+  <>
+    <Header setData={setSearch}/>
+    {data
+    ? <User data={data}/>
+    : <EmptyResult icon={searchIcon} text={"Start with searching a GitHub user"}/>}
+  </>
+  );
 }
 
 export default App;
