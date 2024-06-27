7899b4bbe1ce703e5a3816a6b96d09427691aa1b
denisshapochkin <denis@getfocalpoint.com>
2024-06-27 13:46:13 +0300

    fix: tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-	plugins: [react(), svgr()],
-	css: {
-		postcss: {
-			config: "./postcss.config.cjs",
-		},
-	},
+  plugins: [react(), svgr()],
 });
